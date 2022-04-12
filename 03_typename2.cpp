72c2c60a7acca35c98c00645df29bfdea22d0627
Jaehoon Kim <ohahaha822@gmail.com>
2022-04-12 19:49:27 +0900

    [Typename] - Support various containers. - 서로 다른 타입의 컨테이너 지원

--- 03_typename2.cpp
+++ 03_typename2.cpp
@@ -3,10 +3,14 @@
 #include <list>
 
 template <typename T>
-void print_first_element(std::vector<T> &v)
+void print_first_element(T &v)
 {
-    T n = v.front();
+    typename T::value_type n = v.front(); // "value_type"은 STL 규약.
     std::cout << n << std::endl;
+
+    // c++11 이후로는 아래처럼 auto keyword를 사용할 수도 있음.
+    // auto n2 = v.front();
+    // std::cout << n2 << std::endl;
 }
 
 int main()
