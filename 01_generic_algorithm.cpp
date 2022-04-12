e8d40af67eb5f15d003344fd85ba10b93b286479
Jaehoon Kim <ohahaha822@gmail.com>
2022-04-12 18:27:50 +0900

    [Generic algorithm] - More Generically.   - 기존 구조의 문제점.   - 구간의 타입과 검색 대상의 타입이 연관 됨. -> compile time에 "T"의 타입을 정적화 할 수 없음.   - 'T*" 라고 하면 항상 "Raw pointer 만 사용 가능" -> 스마트 포인터를 사용할 수 없음. (스마트 포인터는 객체 형태)

--- 01_generic_algorithm.cpp
+++ 01_generic_algorithm.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-template <typename T>
-T *find(T *first, T *last, T c)
+template <typename T1, typename T2>
+T1 find(T1 first, T1 last, T2 value)
 {
-    while (first != last && *first != c)
+    while (first != last && *first != value)
         ++first;
     return first == last ? nullptr : first;
 }
@@ -12,7 +12,7 @@ int main()
 {
     double x[10] = {1, 2, 3, 4, 5};
 
-    double *p = find(x, x + 5, 5.0);
+    double *p = find(x, x + 5, 5.0f);
 
     if (p == nullptr)
         std::cout << "not found" << std::endl;
