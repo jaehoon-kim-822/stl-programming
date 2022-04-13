5aa86a61f26e4a24bc0c13e4ea486c8b03cf4390
Jaehoon Kim <ohahaha822@gmail.com>
2022-04-14 08:19:14 +0900

    [function_object] - urand more template

--- 11_function_object2.cpp
+++ 11_function_object2.cpp
@@ -4,10 +4,12 @@
 // 0 ~ 9 사이의 중복되지 않은 난수를 반환하는 함수. 상태가 필요함. 함수에는 상태가 없음.
 // 일반 함수는 상태를 가질 수 없지만, 함수 객체는 상태(데이터)를 가질 수 있다.
 
+template <int COUNT, int BASE>
 class URandom
 {
-    std::bitset<10> bs;
+    std::bitset<COUNT> bs;
     bool recycle;
+
 public:
     URandom(bool b = false) : recycle(b)
     {
@@ -16,25 +18,26 @@ public:
 
     int operator()()
     {
-        if (bs.none())
-        {
-            if (true == recycle)
-            {
-                bs.set();
-            }
-            else
-            {
-                return -1;
-            }
-        }
         int value = -1;
-        while (false == bs.test(value = rand() % 10));
-        bs.reset(value);
-        return value;
+        if (false == bs.none())
+        {
+            while (false == bs.test(value = rand() % COUNT))
+            {
+            }
+            bs.reset(value);
+        }
+        else if (true == recycle)
+        {
+            bs.set();
+        }
+        return BASE + value;
     }
+
+    reset() { bs.set(); }
+    int remain() { return bs.count(); }
 };
 
-URandom urand(true); // 11번 째에 초기화됨. 
+URandom<100, 100> urand(true); // 100 ~ 199의 유일 난수.
 
 int main()
 {
