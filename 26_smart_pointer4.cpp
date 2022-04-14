7ff7948594c4c062305eb941a62bbaad37933160
Jaehoon Kim <ohahaha822@gmail.com>
2022-04-14 16:27:28 +0900

    [smart pointer] - dangling pointer

--- 26_smart_pointer4.cpp
+++ 26_smart_pointer4.cpp
@@ -9,14 +9,20 @@ struct People
     People(std::string n) : name(n) {}
     ~People() { std::cout << name << "detroyed" << std::endl; }
 
-    People *bf; // shared_ptr 대신 primitive pointer 사용.
+    People *bf;
 };
 
 int main()
 {
     std::shared_ptr<People> sp1(new People("kim"));
-    std::shared_ptr<People> sp2(new People("lee"));
+    {
+        std::shared_ptr<People> sp2(new People("lee"));
+        sp1->bf = sp2.get();
+        sp2->bf = sp1.get();
+    }
 
-    sp1->bf = sp2.get();
-    sp2->bf = sp1.get();
+    if (sp1->bf != 0) // dangling pointer 발생.
+    {
+        std::cout << sp1->bf->name << std::endl;
+    }
 }
