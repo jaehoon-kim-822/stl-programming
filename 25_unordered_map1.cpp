d07fb265106dadfc0bce684a401881392afe84aa
Jaehoon Kim <ohahaha822@gmail.com>
2022-04-14 15:29:29 +0900

    [unordered_map] - word ratio app

---
+++ 25_unordered_map1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <list>
+#include <unordered_map>
+#include <algorithm>
+#include "show.h"
+
+int main()
+{
+    std::unordered_map<std::string, std::list<int>> indexMap;
+    std::ifstream f("readme.txt");
+
+    int line = 0;
+    std::string s;
+
+    while (std::getline(f, s))
+    {
+        ++line;
+        std::istringstream iss(s);
+        std::string word;
+        while (iss >> word)
+        {
+            // key가 없으면, pair를 생성하고, value를 반환.
+            // key가 있으면, value 반환.
+            indexMap[word].push_back(line);
+        }
+    }
+
+    for (auto& p : indexMap)
+    {
+        std::cout << p.first << ": ";
+        show(p.second);
+    }
+}
