377348d2f2801028c97ffbfb4861309dbae9f3f4
Jaehoon Kim <ohahaha822@gmail.com>
2022-04-14 16:21:58 +0900

    [smart pointer] - reference count

--- 26_smart_pointer1.cpp
+++ 26_smart_pointer1.cpp
@@ -15,20 +15,38 @@ public:
 template <typename T>
 class Ptr
 {
-    Car *obj;
+    T *obj;
+    int *ref;
 
 public:
-    Ptr(Car *p = 0) : obj(p) {}
-    ~Ptr() { delete obj; }
+    Ptr(T *p = 0) : obj(p)
+    {
+        ref = new int;
+        *ref = 1;
+    }
 
-    Car *operator->() { return obj; }
-    Car &operator*() { return *obj; }
+    Ptr(const Ptr<T> &p) : obj(p.ref), ref(p.ref)
+    {
+        ++(*ref);
+    }
+
+    ~Ptr()
+    {
+        if (nullptr != ref && 0 == --(*ref))
+        {
+            delete obj;
+            delete ref;
+        }
+    }
+
+    T *operator->() { return obj; }
+    T &operator*() { return *obj; }
 };
 
 int main()
 {
     // Ptr의 멤버변수로 Car의 포인터가 있긴 하나, 객체 자체를 Car의 포인터처럼 사용: 스마트 포인터
-    Ptr<int> p = new Car; // Ptr p(new Car);
+    Ptr<Car> p = new Car; // Ptr p(new Car);
 
     p->Go(); // p.operator->()Go() => (p.operator->())->Go()
 
@@ -38,4 +56,9 @@ int main()
     // Ptr p가 local object이기 때문에 함수 종료시 소멸된다. 이 때, 할당된 객체 Car의 소멸자만 호출해주면, 안전하게 메모리 사용을 할 수 있다.
 
     (*p).Go();
+
+    // 스마트 포인터는 반드시 "얕은 복사" 문제를 해결해야 합니다.
+    // 참조 계수 Reference count 를 사용해서 해결한다.
+    Ptr<int> p1 = new int;
+    Ptr<int> p2 = p1;
 }
